c07fb9c6f0c9fb87ce7ae31a2fbf4dfcd9f0e3b8
Williamonyango <ojwangwiliam@gmail.com>
2023-10-05 23:16:13 +0300

    import and modules

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+    a: first number
+    b: second number
+
+    Returns:
+    The return value, a + b
+    """
+    return(a + b)
+
+def sub(a, b):
+    """My substraction function
+
+    Args:
+    a: first number
+    b: second number
+
+    Returns:
+    The return value, a - b
+    """
+    return(a - b)
+def mul(a, b):
+    """My multiplication function
+
+    Args:
+    a: first number
+    b: second number
+
+    Returns:
+    The return value. a * b
+    """
+    return(a * b)
+def div(a, b):
+    """My division
+
+    Args:
+    a: first number
+    b: second number
+
+    Return:
+     The return value. a / b
+    """
+    return int(a/b)
