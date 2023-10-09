62e9413c9d7ac5d13e39d45f1f50a2bd4715d754
Williamonyango <ojwangwiliam@gmail.com>
2023-10-09 23:05:25 +0300

    python Data structures

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 #0-print_list_integer.py
 
-def eleent_at(my_list, idx):
+def print_list_integer(my_list=[]):
     """Print all itegers of a list"""
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
